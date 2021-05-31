7ca2f487b6d481705e4a2c568d91f5e6d24fa543
LAPTOP-FRKOCMT9\FIKI <fiki.abdulrachman@gmail.com>
2021-05-31 10:40:13 +0700

    Change on line 14-15

---
+++ MouseRotation.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotation : MonoBehaviour
+{
+    public bool lockCursor = true;
+    [SerializeField] private float _turnSpeed = 3f;
+
+    private bool m_cursorIsLocked = true;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Mouse X");
+        transform.Rotate(horizontal * _turnSpeed * Vector3.up, Space.World);
+
+        UpdateCursorLock();
+    }
+
+    public void SetCursorLock(bool value)
+    {
+        lockCursor = value;
+        if (!lockCursor)
+        {//we force unlock the cursor if the user disable the cursor locking helper
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void UpdateCursorLock()
+    {
+        //if the user set "lockCursor" we check & properly lock the cursos
+        if (lockCursor)
+            InternalLockUpdate();
+    }
+
+    private void InternalLockUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            m_cursorIsLocked = false;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            m_cursorIsLocked = true;
+        }
+
+        if (m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (!m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
